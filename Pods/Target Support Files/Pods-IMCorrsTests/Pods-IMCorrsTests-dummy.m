d196a265f6f3819b141c10c6fd1fc6440efbede5
Rockyto Sánchez <rockyto@la-MacBook-Pro-de-Rodrigo.local>
2018-09-06 21:14:15 -0500

    primer commit

---
+++ Pods/Target Support Files/Pods-IMCorrsTests/Pods-IMCorrsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IMCorrsTests : NSObject
+@end
+@implementation PodsDummy_Pods_IMCorrsTests
+@end
---
+++ Pods/Target Support Files/Pods-IMCorrsUITests/Pods-IMCorrsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IMCorrsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_IMCorrsUITests
+@end
